36578b2dd9b30c992a241a608f3873c5c4d32527
Anonim-IT <leo3519102018@outlook.com>
2024-12-27 22:07:47 +0100

    modified:   index.html 	modified:   information.html 	modified:   main.js 	modified:   news.html 	modified:   sidebar.html 	modified:   style.css

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-// main.js
-
-// Динамическая загрузка боковой панели
-fetch('sidebar.html')
-  .then(response => response.text())
-  .then(html => {
-    document.getElementById('sidebar').innerHTML = html;
-  });
-
-// Модальное окно
 function showSettings() {
   document.getElementById('settingsModal').style.display = 'flex';
 }
