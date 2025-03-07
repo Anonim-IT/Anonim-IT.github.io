09fe54facb9f47359de4cc849ed9472dccf3eaad
Anonim-IT <leo3519102018@outlook.com>
2025-03-07 04:18:18 +0100

    new file:   Northern_Valley.html 	new file:   Selo/css/styles.css 	new file:   Selo/js/scripts.js 	new file:   Selo/nav/navbar.html

---
+++ Selo/js/scripts.js
@@ -0,0 +1,18 @@
+        // Загрузка навигационного меню
+        fetch("Selo/nav/navbar.html")
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('navbar-container').innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Ошибка при загрузке навигационной панели:', error);
+            });
+  
+        // Получаем элементы меню
+        const menuToggle = document.querySelector('.menu-toggle');
+        const menuList = document.querySelector('.menu-list');
+  
+        // Обработчик клика для открытия и закрытия меню
+        menuToggle.addEventListener('click', () => {
+          menuList.classList.toggle('open'); // Переключаем класс 'open' для отображения/скрытия меню
+        });
